6414616a4e800a96a62403af0efd9e0c19d177f2
jakubhvizdak <hvizdak24@gmail.com>
2025-11-20 18:41:14 +0100

    added interceptor for ignore jwt refresh while using wrong credentials on login page

--- resources/js/main.js
+++ resources/js/main.js
@@ -6,6 +6,23 @@ import axios from 'axios';
 axios.defaults.baseURL = 'http://localhost:8000/api';
 axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('token')}`;
 
+axios.interceptors.response.use(
+    response => response,
+    error => {
+        if (
+            error.response &&
+            error.response.status === 401 &&
+            !error.config.url.includes('/login')
+        ) {
+            localStorage.removeItem('token');
+            localStorage.removeItem('user');
+            window.location.href = '/login';
+        }
+
+        return Promise.reject(error);
+    }
+);
+
 const app = createApp(App);
 
 app.config.globalProperties.$axios = axios;
