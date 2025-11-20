2e315f91608e7e7ed6eaf50d12b41caf3672a5ef
jakubhvizdak <hvizdak24@gmail.com>
2025-11-20 23:50:02 +0100

    fix background color on dashboard

--- resources/views/Dashboard.vue
+++ resources/views/Dashboard.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="min-h-screen bg-gradient-to-br from-slate-50 via-blue-50 to-slate-100">
+    <div class="min-h-screen bg-gradient-to-br from-gray-50 to-gray-100">
         <Navbar />
 
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6 sm:py-8">
