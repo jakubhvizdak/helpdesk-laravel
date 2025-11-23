9e64df3bfa47097a71891b2c3abeeee3c30737e8
jakubhvizdak <hvizdak24@gmail.com>
2025-11-23 17:42:56 +0100

    fix vue app mount and router setup with Vite

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,32 @@
 import '../css/app.css';
 import { createApp } from 'vue';
 import App from './App.vue';
+import router from '../router/index.js';
+import axios from 'axios';
 
-createApp(App).mount('#app');
+axios.defaults.baseURL = 'http://localhost:8000/api';
+axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('token')}`;
+
+axios.interceptors.response.use(
+    response => response,
+    error => {
+        if (
+            error.response &&
+            error.response.status === 401 &&
+            !error.config.url.includes('/login')
+        ) {
+            localStorage.removeItem('token');
+            localStorage.removeItem('user');
+            window.location.href = '/login';
+        }
+        return Promise.reject(error);
+    }
+);
+
+const app = createApp(App);
+
+app.config.globalProperties.$axios = axios;
+
+app.use(router);
+
+app.mount('#app');
