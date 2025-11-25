f3de9f3ecee8e79368e10f68cc247e774ec7dde8
jakubhvizdak <hvizdak24@gmail.com>
2025-11-25 17:01:06 +0100

    optimizing vite buid

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    build: {
+        chunkSizeWarningLimit: 800,
+        minify: 'esbuild',
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
